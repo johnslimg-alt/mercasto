7d6d31b72ed91b879d8b59b96906184511f04258
johnslimg-alt <johnslimg@gmail.com>
2026-04-25 07:10:21 -0600

    hotfix: rollback route analytics tracker
    Rolls back route analytics tracker runtime wiring from main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppWrapper from './App.jsx'
-import AnalyticsRouteTracker from './components/common/AnalyticsRouteTracker.jsx'
 import './index.css'
 
 const rootElement = document.getElementById('root');
@@ -10,7 +9,6 @@ if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
       <BrowserRouter>
-        <AnalyticsRouteTracker />
         <AppWrapper />
       </BrowserRouter>
     </React.StrictMode>
