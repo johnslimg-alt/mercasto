42e8ad62470dc81a2416596e865f12d1e316127b
johnslimg-alt <johnslimg@gmail.com>
2026-04-25 06:59:48 -0600

    feat(analytics): wire route view tracker

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppWrapper from './App.jsx'
+import AnalyticsRouteTracker from './components/common/AnalyticsRouteTracker.jsx'
 import './index.css'
 
 const rootElement = document.getElementById('root');
@@ -9,6 +10,7 @@ if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
       <BrowserRouter>
+        <AnalyticsRouteTracker />
         <AppWrapper />
       </BrowserRouter>
     </React.StrictMode>
