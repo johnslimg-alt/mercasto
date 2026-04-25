ae1ec54643b29702ddfafa6c5e903628931d08a5
johnslimg-alt <johnslimg@gmail.com>
2026-04-25 07:04:08 -0600

    feat(analytics): wire passive interaction tracker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppWrapper from './App.jsx'
 import AnalyticsRouteTracker from './components/common/AnalyticsRouteTracker.jsx'
+import AnalyticsInteractionTracker from './components/common/AnalyticsInteractionTracker.jsx'
 import './index.css'
 
 const rootElement = document.getElementById('root');
@@ -11,6 +12,7 @@ if (rootElement) {
     <React.StrictMode>
       <BrowserRouter>
         <AnalyticsRouteTracker />
+        <AnalyticsInteractionTracker />
         <AppWrapper />
       </BrowserRouter>
     </React.StrictMode>
