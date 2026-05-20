c25c25fbbeb3be75a6f9d452e17911bdfb29cb94
johnslimg-alt <johnslimg@gmail.com>
2026-05-19 22:21:01 -0600

    share route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShareAdController;
+
+Route::get('/share/ads/{id}', ShareAdController::class)->whereNumber('id');
 
 Route::get('/', function () {
     return view('welcome');
