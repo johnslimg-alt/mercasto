335a5cc1d3a84d1a561ac063f2c134580b4e79fd
Mercasto Dev <johnslimg@gmail.com>
2026-06-01 22:32:21 -0600

    fix: disable service worker flicker

--- src/main.jsx
+++ src/main.jsx
@@ -15,9 +15,3 @@ if (rootElement) {
     </React.StrictMode>
   );
 }
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {});
-  });
-}
