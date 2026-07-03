288febf27109267a199445e7f5442fdb52288f42
johnslimg-alt <johnslimg@gmail.com>
2026-07-03 12:23:40 -0600

    Translate seller contact email template

--- backend/resources/views/emails/seller_contact.blade.php
+++ backend/resources/views/emails/seller_contact.blade.php
@@ -1,34 +1,35 @@
 @extends('emails.layout')
 
-@section('subject', $buyerName . " está interesado en tu anuncio '" . $adTitle . "'")
+@section('subject', __('emails.seller_contact.subject', ['buyer' => $buyerName, 'ad' => $adTitle]))
+
+@section('preheader', __('emails.seller_contact.preheader', ['buyer' => $buyerName]))
 
 @section('content')
-    <h1>Tienes una nueva consulta 📩</h1>
+    <h1>{{ __('emails.seller_contact.title') }}</h1>
 
     <p>
-        <strong>{{ $buyerName }}</strong> te ha escrito sobre tu anuncio
-        <strong>"{{ $adTitle }}"</strong>:
+        {!! __('emails.seller_contact.intro_html', [
+            'buyer' => e($buyerName),
+            'ad' => e($adTitle),
+        ]) !!}
     </p>
 
     <div class="message-preview">
         {{ Str::limit($messageBody, 1000) }}
     </div>
 
-    <p>
-        Puedes responder directamente a este correo para contactar a {{ $buyerName }}.
-    </p>
+    <p>{{ __('emails.seller_contact.reply_hint', ['buyer' => $buyerName]) }}</p>
 
     <div class="btn-wrapper">
         <a href="{{ config('app.frontend_url', 'https://mercasto.com') }}/?ad={{ $adId }}" class="btn">
-            Ver anuncio
+            {{ __('emails.seller_contact.button') }}
         </a>
     </div>
 
     <hr class="divider">
 
     <p style="font-size: 13px; color: #94A3B8;">
-        Recibes este correo porque un comprador usó el formulario de contacto de Mercasto.
-        Tu dirección de correo no fue compartida con el comprador.
-        Nunca compartas datos de pago por correo y desconfía de pagos anticipados.
+        {{ __('emails.seller_contact.privacy') }}
+        {{ __('emails.seller_contact.safety') }}
     </p>
 @endsection
