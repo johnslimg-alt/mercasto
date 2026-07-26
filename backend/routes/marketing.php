18e8d25c6bedcb703e7f3e9b0a58cd794cb07233
johnslimg-alt <johnslimg@gmail.com>
2026-07-25 22:23:20 -0600

    Add protected marketing API routes

---
+++ backend/routes/marketing.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\MarketingController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['api', 'auth:sanctum', 'throttle:api'])
+    ->prefix('api/admin/marketing')
+    ->group(function () {
+        Route::get('/meta/status', [MarketingController::class, 'metaStatus']);
+        Route::get('/meta/campaigns', [MarketingController::class, 'metaCampaigns']);
+    });
