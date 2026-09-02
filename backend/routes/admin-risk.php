5d125b7033f9861cd1f8e2267d7e4918834f4fc9
johnslimg-alt <johnslimg@gmail.com>
2026-09-02 00:11:34 -0600

    refactor: remove duplicate risk routes

--- backend/routes/admin-risk.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\AdminFraudRiskController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('throttle:30,1')->get('/risk/ads', [AdminFraudRiskController::class, 'index']);
-Route::middleware('throttle:10,1')->post('/risk/ads/{ad}/analyze', [AdminFraudRiskController::class, 'analyze'])->whereNumber('ad');
-Route::middleware('throttle:2,1')->post('/risk/batch', [AdminFraudRiskController::class, 'batch']);
